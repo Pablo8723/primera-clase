044bbe59deda5f49c42a34a247b7932662eeb490
Pablo Rocha <pabluras_123ggl@hotmail.com>
2023-03-22 20:53:30 -0500

    Primer commit de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,3 +9,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'claseVisualStudioCode';
 }
+
+
+//ENTIDADES DE PRODUCTO
+
+let CODIGO          : number = 0;
+let NOMBRE          : string = '';
+let PESO            : number = 0;
+let FAMILIA         : string = '';
+let FECHA_CADUCIDAD : string = '';
+let UNIDAD          : number = 0;
+let LOTE            : string = '';
+let PVP             : number = 0;
+let COSTO           : number = 0;
+
+
+console.log('hola mundo');
+console.log('hola mundo');
+
+console.log('Hola Mundo Sniper');
+
+console.log(this.codigo);
+
+
---
+++ src/app/models/modeloPrueb.modle.ts
@@ -0,0 +1,18 @@
+export class PruebadeModelo{
+
+    constuctor(
+        public CODIGO: string,
+        public NOMBRE: string,
+        public PESO: string,
+        public FAMILIA: string,
+        public FECHA_CADUCIDAD: string,
+        public UNIDAD: string,
+        public LOTE: string,
+        public PVP: string,
+        public COSTO: string
+
+    ){
+    }
+
+    console.log('');
+}

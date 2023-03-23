3d33f12a453107a12a606662777fe7b8678e1a2c
Pablo Rocha <pabluras_123ggl@hotmail.com>
2023-03-22 21:30:54 -0500

    Prubas

--- src/app/models/modeloPrueb.modle.ts
+++ src/app/models/modeloPrueb.modle.ts
@@ -13,6 +13,19 @@ export class PruebadeModelo{
 
     ){
     }
+    constuctor(
+        public CODIGO: string,
+        public NOMBRE: string,
+        public PESO: string,
+        public FAMILIA: string,
+        public FECHA_CADUCIDAD: string,
+        public UNIDAD: string,
+        public LOTE: string,
+        public PVP: string,
+        public COSTO: string
+
+    ){
+    }
 
     console.log('');
 }
